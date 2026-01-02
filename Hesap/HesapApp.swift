1f90e3c68a5502a923bb2b84994f445ed398dbb0
Engin <enginozy@gmail.com>
2026-01-02 13:48:40 +0300

    Initial Commit

---
+++ Hesap/HesapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HesapApp.swift
+//  Hesap
+//
+//  Created by Engin Özyılmaz on 2.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HesapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
